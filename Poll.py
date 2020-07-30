65c253275915909b179cec17e54979fcabf02266
TiaraSGreen <65461540+TiaraSGreen@users.noreply.github.com>
2020-07-30 00:25:42 -0500

    Add files via upload

--- Poll.py
+++ Poll.py
@@ -34,4 +34,60 @@ with open(election_data,'r') as csvfile:
 
     if candidate in can_name:
         candidate_index = cand_name.indec(candidate)
-        can_vote[candidate_index]=can_vo
+        can_vote[candidate_index]=can_vote[candidate_index]+1
+    else
+        can_name.append(candidate)
+        can_vote.append(1)
+    
+    #percent value indexes and list 
+    voter_stats = []
+    most_votes = can_vote[0]
+    most_votes_index=0
+
+    #find percent of votes per candidate
+    for num in range(len(can_name)):
+        percent_voted = round(can_vote[num]/num_vote*100,2)
+        voter_stats.append(percent_voted)
+        if can_vote[num] > most_votes:
+            most_votes = can_votes[num]
+            print(most_votes)
+            most_votes_index=num
+    winner = can_nam[most_votes_index]
+
+    #print results
+
+    print("Election Results")
+    print("----------------")
+    print(f"Total Votes: {num_vote}")
+
+    #print name, percent, and votes of candidates
+
+    for count in range(len(can_name)):
+        print(f" {can_name[num]}: {voter_stats[num]}% ({can_vote[num]})")
+    
+    #print *drumroll please* winner
+
+    print("----------------")
+    print(f"Winner: {winner}")
+    print("----------------")
+
+    #text file
+
+    file = open("Analysis/poll_analysis.txt", "w")
+
+    file.write("Election Results" + "\n")
+
+    file.write("-------------------" + "\n")
+
+    file.write("Total Votes: " + str(num_vote) + "\n")
+
+    for count in range(len(can_name)):
+        file write(f"{can_name[num]}: {voter_stats[num]}% ({can_vote[num]})" + "\n")
+    
+    file.write("--------------------")
+    file.write(f'Winner:{winner}\n')
+    file.write("-------------------" + "\n")
+
+    fil.close()
+
+
